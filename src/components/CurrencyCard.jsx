635b9a62728f8f618588251273e500f3024d115d
Enes Tufekci <enesxtufekci@gmail.com>
2024-10-30 09:45:52 +0100

    fix react

--- src/components/CurrencyCard.jsx
+++ src/components/CurrencyCard.jsx
@@ -1,72 +1,78 @@
-import CurrencyCardContainer from "./currencyCard/CurrencyCardContainer"
-import "../assets/styles/currency_card.css"
-import CurrencyCardPart from "./currencyCard/CurrencyCardPart"
+import CurrencyCardContainer from "./currencyCard/CurrencyCardContainer";
+import "../assets/styles/currency_card.css";
+import CurrencyCardPart from "./currencyCard/CurrencyCardPart";
 import { MdCompareArrows } from "react-icons/md";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import env from "react-dotenv";
 
 function CurrencyCard() {
-    const [data, setData] = useState({});
+  const [data, setData] = useState({});
+  const [fromInput, setFromInput] = useState(0);
+  const [toInput, setToInput] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("USD");
 
-    useEffect(() => {
-        getData()
-    }, [])
+  useEffect(() => {
+    getData();
+  }, []);
 
+  const getData = async () => {
+    const response = await axios.get(import.meta.env.VITE_BASE_URL, {
+      headers: {
+        "Content-Type": "application/json",
+        apikey: import.meta.env.VITE_API_KEY,
+        base_currency: "USD",
+      },
+    });
+    console.log(response.data.data);
 
-    const getData = async () => {
-        const response = await axios.get(env.BASE_URL, {
-            headers: {
-                "Content-Type": "application/json",
-                "apikey": env.API_KEY,
-                "base_currency": "USD"
-            }
-        })
-        setData(response.data.data);
+    setData(response.data.data);
+  };
+
+  const handleSubmit = async () => {
+    if (!isNaN(Number(fromInput)) && fromInput > 0) {
+      let oneCurrency = 1 / data[fromCurrency];
+      let result = fromInput * (oneCurrency * data[toCurrency]);
+      setToInput(result);
     }
+  };
 
+  const handleValues = () => {
+    const tempCurrency = fromCurrency;
+    setFromCurrency(toCurrency);
+    setToCurrency(tempCurrency);
+    const tempInput = fromInput;
+    setFromInput(toInput ?? 0);
+    setToInput(tempInput ?? 0);
+  };
 
-    const handleSubmit = async () => {
-        const from = document.querySelector('#fromInput');
-        const fromDropdown = document.querySelector("#fromCurrency")
-        const to = document.querySelector("#toInput");
-        const toDropdown = document.querySelector("#toCurrency");
-
-        if (!isNaN(Number(from.value)) && from.value > 0) {
-            let oneCurrency = 1 / data[`${fromDropdown.value}`]
-            let result = from.value * (oneCurrency * data[`${toDropdown.value}`])
-            to.value = result.toFixed(2);
-        }
-    }
-    const handleValues = () => {
-        const from = document.querySelector('#fromInput');
-        const fromDropdown = document.querySelector("#fromCurrency")
-        const to = document.querySelector("#toInput");
-        const toDropdown = document.querySelector("#toCurrency");
-
-        let privateValue = from.value;
-        from.value = to.value;
-        to.value = privateValue;
-        privateValue = fromDropdown.value
-        fromDropdown.value = toDropdown.value;
-        toDropdown.value = privateValue
-    }
-
-    return (
-        <>
-            <CurrencyCardContainer>
-                <div className="currency__card--title">
-                    Exchange App
-                </div>
-                <div className="currency__input--container">
-                    <CurrencyCardPart inputId="fromInput" dropdownId="fromCurrency" />
-                    <MdCompareArrows onClick={handleValues} className="arrow-ico" />
-                    <CurrencyCardPart inputId="toInput" dropdownId="toCurrency" />
-                </div>
-                <button onClick={handleSubmit} id="submitBtn">Convert</button>
-            </CurrencyCardContainer>
-        </>
-    )
+  return (
+    <>
+      <CurrencyCardContainer>
+        <div className="currency__card--title">Exchange App</div>
+        <div className="currency__input--container">
+          <CurrencyCardPart
+            currencies={Object.keys(data)}
+            value={fromInput}
+            currency={fromCurrency}
+            onValueChange={(value) => setFromInput(value)}
+            onCurrencyChange={(currency) => setFromCurrency(currency)}
+          />
+          <MdCompareArrows onClick={handleValues} className="arrow-ico" />
+          <CurrencyCardPart
+            currencies={Object.keys(data)}
+            value={toInput}
+            currency={toCurrency}
+            onValueChange={(value) => setToInput(value)}
+            onCurrencyChange={(currency) => setToCurrency(currency)}
+          />
+        </div>
+        <button onClick={handleSubmit} id="submitBtn">
+          Convert
+        </button>
+      </CurrencyCardContainer>
+    </>
+  );
 }
 
-export default CurrencyCard
+export default CurrencyCard;
--- src/components/currencyCard/CurrencyCardPart.jsx
+++ src/components/currencyCard/CurrencyCardPart.jsx
@@ -1,32 +1,34 @@
-import { useState } from "react";
-
-
-
-// async function getKeys() {
-//     let data = await getData();
-//     console.log(data)
-//     return Object.keys(data);
-// }
-
 function CurrencyCardPart(props) {
-    let val = []
-    const { inputId, dropdownId } = props;
-    const [currencies, setCurrencies] = useState(['USD', 'TRY', 'EUR']);
+  const {
+    onCurrencyChange,
+    currency,
+    onValueChange,
+    value,
+    currencies = [],
+  } = props;
 
-
-
-
-    return (
-        <div className="currency__part">
-            <select id={dropdownId} name="currency_dropdown" className="currency__dropdown">
-
-                {currencies.map((item, key) => <option key={key} value={item} className="currency__option--option">{item}</option>)}
-
-            </select>
-            <input type="number" id={inputId} className="currency__input">
-            </input>
-        </div>
-    )
+  return (
+    <div className="currency__part">
+      <select
+        name="currency_dropdown"
+        className="currency__dropdown"
+        value={currency}
+        onChange={(e) => onCurrencyChange(e.target.value)}
+      >
+        {currencies.map((item, key) => (
+          <option key={key} value={item} className="currency__option--option">
+            {item}
+          </option>
+        ))}
+      </select>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => onValueChange(e.target.value)}
+        className="currency__input"
+      ></input>
+    </div>
+  );
 }
 
-export default CurrencyCardPart
+export default CurrencyCardPart;
